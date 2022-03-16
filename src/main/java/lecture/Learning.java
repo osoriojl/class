88a6429e1630b0093279c598eeb89a2bce27fe93
osoriojl <osoriojl@outlook.com>
2022-03-16 10:42:59 -0400

    Class answer

---
+++ src/main/java/lecture/Learning.java
@@ -0,0 +1,24 @@
+package lecture;
+
+public class Learning {
+    public static void main(String[] args) {
+        String item = "Mug";
+        int qty = 20;
+        float price = 5.0f;
+
+        float total = (int) qty * price;
+
+        System.out.printf("Product\t    Qty\t      Price\t      Total\n");
+        System.out.printf("----\t   ----\t      ----\t      ----\n");
+        System.out.println(item + "          " + qty + "        " + price + "        " + total);
+
+
+
+
+
+
+
+
+
+    }
+}
